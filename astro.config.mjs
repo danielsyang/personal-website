a829c2b6a574f8e489827b58ee1f5126dc2276d1
Daniel Yang <shu.minyang@hotmail.com>
2023-10-22 17:58:23 -0400

    fix: using a different adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
 import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
   output: "server",
-  adapter: node({
-    mode: "hybrid"
-  })
+  adapter: vercel()
 });
